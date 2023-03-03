29f64dda92e6cbfbacf000c7bbf21f6d9cbe83c4
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-03-03 20:03:09 +0000

    modular plugin config files

--- init.lua
+++ init.lua
@@ -1,40 +1,5 @@
+require('options')
 require('plugins')
-
-vim.keymap.set("n", "<Space>", "<Nop>", {silent = true})
-vim.g.mapleader = " "
-
--- general vim settings
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-vim.opt.wrap = false
-vim.opt.breakindent = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.guicursor = ""
-vim.scrolloff = 8
-vim.opt.termguicolors = true
-vim.opt.colorcolumn = "80"
-
--- telescope bindings
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>sf', function()
-  builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
-
--- nerdtree bindings
-vim.keymap.set('n', '<F2>', ':NERDTreeToggle<CR>', {})
-
--- lsp-zero setup
---local lsp = require('lsp-zero').preset({
---  name = 'recommended',
---  sign_icons = {},
---})
---lsp.setup()
+require('telescopeConfig')
+require('nerdtreeConfig')
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,45 +4,18 @@ return require('packer').startup(function(use)
   -- plugin manager - Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  -- fuzzy finder - telescope
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  -- syntax highlighing - treesitter
   use {
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate'
   }
 
-  -- file explorer - nerdtree
   use {'scrooloose/nerdtree'}
 
-  -- lsp - lsp-zero
---  use {
---  'VonHeikemen/lsp-zero.nvim',
---  branch = 'v1.x',
---  requires = {
---    -- LSP Support
---    {'neovim/nvim-lspconfig'},             -- Required
---    {'williamboman/mason.nvim'},           -- Optional
---    {'williamboman/mason-lspconfig.nvim'}, -- Optional
---
---    -- Autocompletion
---    {'hrsh7th/nvim-cmp'},         -- Required
---    {'hrsh7th/cmp-nvim-lsp'},     -- Required
---    {'hrsh7th/cmp-buffer'},       -- Optional
---    {'hrsh7th/cmp-path'},         -- Optional
---    {'saadparwaiz1/cmp_luasnip'}, -- Optional
---    {'hrsh7th/cmp-nvim-lua'},     -- Optional
---
---    -- Snippets
---    {'L3MON4D3/LuaSnip'},             -- Required
---    {'rafamadriz/friendly-snippets'}, -- Optional
---  }
---}
-  
   -- colours
   use { 'folke/tokyonight.nvim' }
   vim.cmd('colorscheme tokyonight')
