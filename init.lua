c83d60bb68a6304090801fc6b3bebe91eed89b5e
Matthew Kandiah <matthew.kandiah@glean.co>
2023-05-02 20:42:45 +0100

    modularize

--- init.lua
+++ init.lua
@@ -1,16 +1,2 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+require("core.keymaps")
 
-vim.opt.backspace = '2'
-vim.opt.showcmd = true
-vim.opt.laststatus = 2
-vim.opt.autowrite = true
-vim.opt.cursorline = true
-vim.opt.autoread = true
-
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.shiftround = true
-vim.opt.expandtab = true
-
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
