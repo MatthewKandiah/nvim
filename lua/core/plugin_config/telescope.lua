5c649d53dc8d651cf10f52b0e35b6f8ac1ce64e3
Matthew Kandiah <matthew.kandiah@glean.co>
2023-05-03 19:52:22 +0100

    add some telescope lsp keymaps - they sound pretty neat

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -3,3 +3,7 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>sf', builtin.live_grep, {})
 
+-- lsp pickers - think these won't work for now, but sound cool for later
+vim.keymap.set('n', '<leader>d', builtin.lsp_definitions, {})
+vim.keymap.set('n', '<leader>t', builtin.lsp_type_definitions, {})
+
