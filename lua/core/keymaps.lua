1ddee33652682e3520812d1d11f5e8858e62ea02
Matthew Kandiah <matthew.kandiah@glean.co>
2023-05-20 13:06:51 +0100

    numbers, scrolloff and copy-paste to/from system clipboard

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,3 +1,4 @@
+vim.keymap.set('n', '<SPACE>', 'Nop')
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -12,8 +13,16 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.keymap.set('n', '<leader>e', ':wqa<CR>')
 vim.keymap.set('n', '<F2>', vim.diagnostic.goto_next, {})
 vim.keymap.set('n', '<F3>', vim.diagnostic.goto_prev, {})
+
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>p', '"+p')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>p', '"+p')
