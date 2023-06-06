c0b5c06e1ccda94b7cac64b47905d249f6438ddf
Matthew Kandiah <matthew.kandiah@glean.co>
2023-06-06 08:09:21 +0100

    fix hover keymap by avoiding clash

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -18,7 +18,7 @@ local on_attach = function(_, _)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
   vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
   vim.keymap.set('n', 'gt', require('telescope.builtin').lsp_type_definitions, {})
-  vim.keymap.set('n', '<N>', vim.lsp.buf.hover, {})
+  vim.keymap.set('n', '<K>', vim.lsp.buf.hover, {})
 end
 
 require('mason-lspconfig').setup_handlers({
