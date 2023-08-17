c449deb563d01b3390eb2a416f208505056246b4
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-08-17 21:04:02 +0100

    split treesitter context setup into a separate file

--- init.lua
+++ init.lua
@@ -6,29 +6,7 @@ vim.g.maplocalleader = ' '
 
 require('dependencies')
 require('debugging')
-
--- probably won't use these much, but I think the exact same config should work well
--- dap.configurations.c = dap.configurations.cpp
--- dap.configuration.rust = dap.configurations.cpp
-
--- treesitter context
-require 'treesitter-context'.setup {
-  enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
-  max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit.
-  min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-  line_numbers = true,
-  multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
-  trim_scope = 'outer',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-  mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
-  -- Separator between context and content. Should be a single character string, like '-'.
-  -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-  separator = nil,
-  zindex = 20,     -- The Z-index of the context window
-  on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-}
-vim.keymap.set('n', '[c', function()
-  require('treesitter-context').go_to_context()
-end, { desc = { 'Go to [c]ontext' } })
+require('ts-context')
 
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
