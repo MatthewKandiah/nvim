4e4d4c732151308f6fb912726f6541f73837dd8c
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-08-17 21:07:34 +0100

    split vim options into separate file

--- init.lua
+++ init.lua
@@ -5,30 +5,9 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 require('dependencies')
+require('options')
 require('debugging')
 require('ts-context')
-
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
-
--- Set highlight on search
-vim.o.hlsearch = false
-vim.o.incsearch = true
-
--- Make line numbers default
-vim.wo.number = true
-vim.wo.relativenumber = true
-vim.o.scrolloff = 12
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
--- tabs
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
-vim.o.smarttab = true
-vim.o.expandtab = false
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
@@ -36,34 +15,7 @@ vim.o.expandtab = false
 -- vim.o.clipboard = 'unnamedplus'
 vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y')
 vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p')
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
-
--- auto writing saves might give better instant diagnostic feedback?
--- vim.o.autowrite = true
--- vim.o.autowriteall = true
-
+--
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
