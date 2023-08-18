0cc8ac193d50e7813a07e5ab3445c0c0f1a71b0b
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-08-18 08:18:19 +0100

    finished splitting up init.lua!

--- init.lua
+++ init.lua
@@ -9,12 +9,11 @@ require('options')
 require('debugging')
 require('ts-context')
 require('keymaps')
+require('commands')
 require('telescope-setup')
 require('treesitter-setup')
 require('lsp-setup')
 require('cmp-setup')
--- convert to hex for viewing binary files
-vim.api.nvim_create_user_command('HexView', '%!xxd', { desc = 'convert current buffer to hex view' })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
