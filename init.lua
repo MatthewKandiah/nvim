ce3685d3e1599d9754f8b14a06aea649b5e99a20
Matthew Kandiah <mj.kandiah@googlemail.com>
2023-12-09 15:33:29 +0000

    more yanking / pasting keymaps

--- init.lua
+++ init.lua
@@ -89,7 +89,9 @@ require('lazy').setup({
 
 -- copy to clipboard
 vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y')
+vim.keymap.set({ 'n', 'v' }, '<leader>Y', '"+Y')
 vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p')
+vim.keymap.set({ 'n', 'v' }, '<leader>P', '"+P')
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- yank group highlight
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
