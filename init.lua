d6a3aa75106d3c43effa53d4a4f2acb9032bb60b
Matthew Kandiah <mj.kandiah@googlemail.com>
2025-06-15 14:33:16 +0100

    pull out lazy-bootstrap

--- init.lua
+++ init.lua
@@ -1,13 +1,4 @@
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
+require('lazy-bootstrap')
 require('lazy').setup({
   'romainl/Apprentice',
   'thimc/gruber-darker.nvim',
