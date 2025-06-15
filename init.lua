94edebbf54f4a64d48ea1748dae7aeea19ee0ec8
Matthew Kandiah <mj.kandiah@googlemail.com>
2025-06-15 19:58:09 +0100

    splitting dependency from config

--- init.lua
+++ init.lua
@@ -19,24 +19,6 @@ require('lazy').setup({
         'nvim-telescope/telescope-ui-select.nvim',
       },
     },
-    config = function()
-      require('telescope').setup {
-        extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
-          },
-        },
-      }
-      pcall(require('telescope').load_extension, 'fzf')
-      pcall(require('telescope').load_extension, 'ui-select')
-      local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sf', builtin.find_files)
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep)
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics)
-      vim.keymap.set('n', '<leader>sr', builtin.resume)
-      vim.keymap.set('n', '<leader>?', builtin.oldfiles)
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers)
-    end
   },
   {
     'neovim/nvim-lspconfig',
@@ -108,3 +90,4 @@ require('gitsigns').setup({
     changedelete = { text = '~' },
   },
 })
+require('telescope-config')
